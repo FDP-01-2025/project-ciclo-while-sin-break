09db953ad590e9e89cad685b5dad9b0779110d94
kikezz12 <00208424@uca.edu.sv>
2025-06-18 16:17:01 -0600

    actualización de funciones

--- shot.h
+++ shot.h
@@ -6,9 +6,9 @@ using namespace std;
 
 // Esta función marca un disparo en el tablero: 'X' si acierta, 'O' si falla.
 // Devuelve true si fue impacto, false si fue agua o repetido.
-bool makeshot(char tablero[10][10], int fila, int columna);
+bool makeshot(char board[10][10], int fila, int columna);
 
 // Esta función verifica si quedan barcos ('B') en el tablero.
-bool quedanBarcos(char tablero[10][10]);
+bool makeshot(char board[10][10]);
 
 #endif
