4ecc5c996a2df30763595456d898cfee1fe40d07
Hwrnandezz <00062625@uca.edu.sv>
2025-06-30 16:40:46 -0600

    se agrego un logro mas y se cambiaeron variables en .h .cpp

--- src/achievements.cpp
+++ src/achievements.cpp
@@ -2,19 +2,20 @@
 #include "achievements.h"
 
 using namespace std;
-
-void checkAchievements(const string& currentPlayer ,bool hit, int hit_consecutive, int turn, bool playerwin, bool playerlose, int ships_destroye)
+void checkAchievements(const string& currentPlayer, bool hit, int hit_consecutive, int turn, bool playerwin, bool playerlose, int ships_destroyed)
 {
     cout << "\n--- Achievements unlocked ---\n";
 
-    /*Este logro se mostrara cuando un jugador realice 5 aciertos seguidos
-     */
     if (hit_consecutive >= 5)
     {
-        cout << currentPlayer << "Sniper: 5 hits in a row.\n";
+        cout << currentPlayer << " Sniper: 5 hits in a row.\n";
     }
-    if (playerwin && ships_destroye == 0)
+    if (playerwin && ships_destroyed == 0)
     {
-        cout << currentPlayer << "“Undefeated”: to win without losing a single ship.\n";
+        cout << currentPlayer << "“ Undefeated”: to win without losing a single ship.\n";
+    }
+    if (playerlose && turn < 10)
+    {
+        cout << currentPlayer << "“ Total disaster”: losing in less than 10 turns.\n";
     }
 }
--- src/achievements.h
+++ src/achievements.h
@@ -3,8 +3,10 @@
 #include <iostream>
 using namespace std;
 
-void checkAchievements(const string& currentPlayer ,bool hit, int hit_consecutive, int turn, bool playerwin, bool playerlose,
-                       int ships_destroye); /*Se utilizaran estas variables para poder asignar los
+
+void checkAchievements(const string& currentPlayer, bool hit, int hit_consecutive, int turn,
+                       bool playerwin, bool playerlose, int ships_destroyed);
+                        /*Se utilizaran estas variables para poder asignar los
                        siguientes logros (se han especificado en el documento):
                        ● “Francotirador”: 5 aciertos seguidos.
                        ● “Invicto”: ganar sin perder un solo barco.
