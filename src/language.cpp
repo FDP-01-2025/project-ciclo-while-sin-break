87a36810c8e0ad49294befe85ad8915db8c3844f
magaly351 <00209425@uca.edu.sv>
2025-07-02 18:57:25 -0600

    terminar language.cpp

--- src/language.cpp
+++ src/language.cpp
@@ -41,26 +41,63 @@ static std::unordered_map<std::string, std::string> english = {
     {"name1", "Enter player 1 name: "},
     {"name2", "Enter player 2 name: "},
     {"name-ships", ", place your ships"},
-    {"name1", "Enter player 1 name"},
+    //{"name1", "Enter player 1 name"},
 
     //achievements
     {"ach_unlocked", "---Achievements unlocked---"},
     {"sniper", "Sniper: 5 hits in a row"},
-    {"undefeaded", "Undefeated: to win without losing a single ship"},
+    {"undefeated", "Undefeated: to win without losing a single ship"},
     {"disaster", "Total disaster: losing in less than 10 turns"},
-    {"lucky", "Lucky Shot: Destroy a ship whith a sigle shot."},
+    {"lucky", "Lucky Shot: Destroy a ship whith a sigle shot."}
     
 };
 
 static std::unordered_map<std::string, std::string> spanish = {
-    {"welcome", "Bienvenido a Battleship"},
-    {"enter_name", "Ingresa tu nombre de jugador: "},
-    {"place_ships", "Coloca tus barcos"},
-    {"turn", "Turno"},
+   //main menu
+    {"g_options", "opciones de juego"},
+    {"pc_player", "1. Juega nueva partida"},
+    {"multip", "2. Juega multiplayer"},
+    {"load_game", "3. Cargar partida guardada"},
+    {"language", "4. Escoja idioma"},
+    {"exit", "5. Salir del juego"},
+
+    //board
+    {"ship_size", "Colocando barcos de tamanno "},
+    {"sym", "(Simbolo '"},
+    {"e_starting", "Introduzca las coordenadas de inicio (e.g): "},
+    {"direction", "Direccion (H para horizontal, V para vertical: "},
+    {"invali_coor", "Coordinadas invalidas. Intente nuevamente"},
+    {"invali_dire", "Direccion invalida. Use H o V"},
+    {"no_place", "No puede colocar barcos ahí. Intente una nueva posicion "},
+
+    //pc-vs_player
+    {"name", "Inrtroduzca su nombre de jugador: "},
+    {"p_ships", "Coloque sus barcos"},
+    {"pc_ships", "La computadora esta colocando sus barcos..."},
+    {"turn_", "'s turno (Turon #"},
+    {"coordinates", "Introduzca coordenadas (e.g) o Z para guardar y salir: "},
+    {"g_c", "Partida guardada. Regresando al menu..."},
+    {"input_lng", "Longitud de entrada no valida. Intenta de nuevo"},
+    {"invalid_r", "Fila invalida. Use A-J"},
+    {"invalid_c", "Numero de columna invalida"},
+    {"col_rang", "Columna fuera de rango. Use numeros 1-10"},
+    {"pc_attc", "Computadora ataco a "},
     {"hit", "¡Impacto!"},
     {"miss", "Fallaste..."},
-    {"win", "¡gana el juego!"},
-    {"enter_coords", "Ingresa coordenadas (por ej. G5) o Z para guardar y salir: "}
+    {"win", "¡GANAS LA PARTIDA!"},
+
+    //multiplayer
+    {"name1", "Introduzca el nombre de jugador 1: "},
+    {"name2", "Introduzca el nombre de jugador 2: "},
+    {"name-ships", ", coloque sus barcos"},
+    //{"name1", "Enter player 1 name"},
+
+    //achievements
+    {"ach_unlocked", "---Logros desbloqueados---"},
+    {"sniper", "Francotirador: 5 golpes en una fila"},
+    {"undefeated", "Invicto: por ganar sin perder ni un barco"},
+    {"disaster", "Desastre total: perdiendo en menos de 10 turnos"},
+    {"lucky", "Golpe de suerte: Destruye un barco con un solo disparo"}
 };
 
 void setLanguage(Language lang) {
